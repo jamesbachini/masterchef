335a5a618f716736583c10a616d9eda7d7f479b1
James Bachini <james@mcambridge.com>
2022-01-19 23:13:28 +0000

    Tests completing, initial setup done

--- test/masterchef-tests.js
+++ test/masterchef-tests.js
@@ -7,31 +7,41 @@ describe("MasterChef",  () => {
   before(async function () {
     [owner,developer,user1,user2] = await ethers.getSigners();
     const MyToken = await ethers.getContractFactory("MyToken");
-    mytoken = await MyToken.deploy(100);
+    mytoken = await MyToken.deploy(ethers.utils.parseEther('100'));
     await mytoken.deployed();
     const startBlock = await ethers.provider.getBlockNumber();
     const endBlock = startBlock + 86400;
     console.log('      Start Block: ',startBlock);
     const MasterChef = await ethers.getContractFactory("MasterChef");
-    masterchef = await MasterChef.deploy(mytoken.address,developer.address,user1.address,100,startBlock,endBlock);
+    masterchef = await MasterChef.deploy(mytoken.address,developer.address,user1.address,ethers.utils.parseEther('100'),startBlock,endBlock);
     await masterchef.deployed();
+
   });
+
+  it("Need to transfer ownership of token to masterchef contract", async () => {
+    mytoken.transferOwnership(masterchef.address);
+  });
+
+
 
   it("Should add a staking pool", async () => {
     masterchef.add(1,mytoken.address,true);
   });
 
   it("Should try staking", async () => {
-    await mytoken.transfer(user1.address,20);
-    const pid = 1; // ?
-
-    /*
-    masterchef.connect(user1).deposit(pid,10);
-    await ethers.provider.send("evm_increaseTime", [24 * 60 * 60]); // 1 day
-    const user1Balance = await mytoken.balanceOf(user1.address);
-    console.log('      One Day Rewards: ',user1Balance - 20);
-    expect(await mytoken.totalSupply()).to.gt(20); 
-    */
+    await mytoken.transfer(user1.address,ethers.utils.parseEther('20'));
+    const pid = 0;
+    await mytoken.connect(user1).approve(masterchef.address,ethers.utils.parseEther('10'));
+    await masterchef.connect(user1).deposit(pid,ethers.utils.parseEther('10'));
+    await ethers.provider.send("evm_increaseTime", [1 * 24 * 60 * 60]); // ~1 days
+    let user1Balance = await mytoken.balanceOf(user1.address);
+    console.log('      User1 Balance: ',ethers.utils.formatEther(user1Balance).toString());
+    await ethers.provider.send("evm_increaseTime", [1 * 24 * 60 * 60]); // ~1 days
+    await masterchef.connect(user1).withdraw(pid,ethers.utils.parseEther('10'));
+    user1Balance = await mytoken.balanceOf(user1.address);
+    console.log('      User1 Balance: ',ethers.utils.formatEther(user1Balance).toString());
+    
+    expect(await mytoken.totalSupply()).to.gt(20);
   });
 
 });
